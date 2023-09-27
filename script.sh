a08e44e4bb791d920554466fee415a1a795846b5
Bartixxx32 <mcbplay1@gmail.com>
2023-09-27 00:46:06 +0000

    revert

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 git clone -b main --depth=1 https://gitlab.com/nikgapps/13.git 13/
 git clone -b master --depth=1 https://github.com/nikgapps/overlays_T.git overlays_T/
-nikgapps --androidVersion $1 --packageList $2
+nikgapps --androidVersion 13 --packageList $1
