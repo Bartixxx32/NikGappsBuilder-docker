35bbfd466ecc2d3dfc951cdb3ed2a0f8d778a5a4
Bartixxx32 <mcbplay1@gmail.com>
2023-09-27 04:24:21 +0200

    forgot

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ if [ "$#" -ne 2 ]; then
     echo "Usage: <Android version> <Gapps variant>"
     exit 1
 fi
-docker run -v $PWD/Releases:/app/Releases -v $PWD/Data/$1:/app/$1 -it test $1 $2
+docker run -v $PWD/Releases:/app/Releases -v $PWD/Data/$1:/app/$1 -it ghcr.io/bartixxx32/nikgappsbuilder-docker:main $1 $2
