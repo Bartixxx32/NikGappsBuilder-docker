05dbb21e668629312f51ca68407bdaab9c81badb
Bartixxx32 <mcbplay1@gmail.com>
2023-09-27 17:04:57 +0000

    beautiful dialog menu

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ WORKDIR /app
 # Install required packages in a single RUN command to reduce layer size
 RUN apt-get update && \
     DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
-    python3 python3-pip aapt gcc python3-dev git git-lfs openjdk-8-jdk apktool && \
+    python3 python3-pip aapt gcc python3-dev git git-lfs openjdk-8-jdk apktool dialog && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
 
@@ -13,11 +13,12 @@ RUN apt-get update && \
 RUN git config --global user.name "Example" && \
     git config --global user.email "example@example.com"
 
-# Copy the script and make it executable
-COPY script.sh .
-RUN chmod +x script.sh
 
 # Install NikGapps using pip
 RUN python3 -m pip install NikGapps
+
+# Copy the script and make it executable
+COPY script.sh .
+RUN chmod +x script.sh
 
 ENTRYPOINT ["bash", "script.sh"]
--- script.sh
+++ script.sh
@@ -1,3 +1,74 @@
-git clone -b main --depth=1 https://gitlab.com/nikgapps/$1.git $1/
-#git pull $1/
-nikgapps --androidVersion $1 --packageList $2
+#!/bin/bash
+
+# Function to validate if a string contains only numbers and is within the specified range
+validate_input() {
+  input="$1"
+  # Regular expression to match digits
+  digit_pattern="^[0-9]+$"
+
+  if [[ ! "$input" =~ $digit_pattern ]]; then
+    echo "Error: First argument must contain only numbers."
+    exit 1
+  fi
+
+  if ((input < 10 || input > 13)); then
+    echo "Error: First argument must be in the range from 10 to 13."
+    exit 1
+  fi
+}
+
+# Function to handle Ctrl+C
+ctrl_c() {
+  echo "Ctrl+C pressed. Exiting..."
+  exit 1
+}
+
+# Trap Ctrl+C and execute the ctrl_c function
+trap ctrl_c INT
+
+# Check if command-line arguments are provided
+if [ $# -eq 2 ]; then
+  # If two arguments are provided, use them directly
+  validate_input "$1"
+  first_page_choice="$1"
+  second_page_choice="$2"
+else
+  # Display the first page using dialog
+  dialog --clear --backtitle "CLI GUI" \
+    --title "Android Version" \
+    --menu "Select an Android version:" 15 40 3 \
+    10 "Android 10" \
+    11 "Android 11" \
+    12 "Android 12" \
+    13 "Android 13" 2> /tmp/first_page_choice || {
+    echo "Dialog canceled. Exiting..."
+    exit 1
+  }
+
+  # Capture the choice made on the first page
+  first_page_choice=$(cat /tmp/first_page_choice)
+  rm /tmp/first_page_choice
+
+  # Display the second page using dialog
+  dialog --clear --backtitle "CLI GUI" \
+    --title "Gapps Variant" \
+    --menu "Select an gapps variant:" 15 40 3 \
+    core "Core" \
+    basic "Basic" \
+    omni "Omni" \
+    stock "stock" \
+    go "Go" \
+    full "Full" 2> /tmp/second_page_choice || {
+    echo "Dialog canceled. Exiting..."
+    exit 1
+  }
+
+  # Capture the choice made on the second page
+  second_page_choice=$(cat /tmp/second_page_choice)
+  rm /tmp/second_page_choice
+fi
+
+# You can now use $first_page_choice and $second_page_choice in your script as needed
+git clone -b main --depth=1 https://gitlab.com/nikgapps/$first_page_choice.git $first_page_choice/
+git pull $first_page_choice/
+nikgapps --androidVersion $first_page_choice --packageList $second_page_choice
